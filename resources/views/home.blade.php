c0ae726b85ab218a57f781fe171b000c58b1aed1
Hamza Hameed <hamzahameed@gmail.com>
2023-11-30 21:26:09 +0500

    fix minor issues

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-12">
                 @php
-                    $ordersCount = count($orders);
+                    $ordersCount = 0;
                 @endphp
                 @component('components.statistics.index')
                     @slot('title') Total orders @endslot
@@ -26,24 +26,22 @@
                 @endif
             </div>
             <div class="col-lg-6 col-md-6 col-sm-12">
-                @foreach ($orders as $item)
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Top 5 orders</h4>
-                        </div>
-                        <div class="card-body">
-                            @foreach ($orders as $order)
-                                <div class="mb-4">
-                                    <div class="text-small float-right font-weight-bold text-muted">{{ $order->commission }}</div>
-                                    <div class="font-weight-bold mb-1">{{ $order->order_no }}</div>
-                                    <div class="progress" data-height="3">
-                                        <div class="progress-bar" role="progressbar" data-width="{{ $percent ?? 50 }}%" aria-valuenow="{{ $percent ?? 50 }}" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>            
-                            @endforeach
-                        </div>
-                    </div>                                   
-                @endforeach
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Top 5 orders</h4>
+                    </div>
+                    <div class="card-body">
+                        @foreach ($orders as $order)
+                            <div class="mb-4">
+                                <div class="text-small float-right font-weight-bold text-muted">{{ $order->commission }}</div>
+                                <div class="font-weight-bold mb-1">{{ $order->order_no }}</div>
+                                <div class="progress" data-height="3">
+                                    <div class="progress-bar" role="progressbar" data-width="{{ $percent ?? 50 }}%" aria-valuenow="{{ $percent ?? 50 }}" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>            
+                        @endforeach
+                    </div>
+                </div>    
             </div>
         </div>    
     </section>
