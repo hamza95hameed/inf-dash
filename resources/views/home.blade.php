a1e53b0526cfbed0b8a08625f182bd0114269388
Hamza Hameed <hamzahameed@gmail.com>
2023-11-30 21:26:39 +0500

    fix minor issues

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,7 +31,7 @@
                         <h4>Top 5 orders</h4>
                     </div>
                     <div class="card-body">
-                        @foreach ($orders as $order)
+                        @foreach (@$orders as $order)
                             <div class="mb-4">
                                 <div class="text-small float-right font-weight-bold text-muted">{{ $order->commission }}</div>
                                 <div class="font-weight-bold mb-1">{{ $order->order_no }}</div>
