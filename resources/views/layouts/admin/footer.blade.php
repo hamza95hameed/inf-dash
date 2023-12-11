9afd1fb75b773fff0606526f5cc0e8baa8793cfc
Hamza Hameed <hamzahameed@gmail.com>
2023-12-11 16:06:39 +0500

    fix minor issues

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="main-footer">
-    <div class="footer-left text-dark">
-        Copyright &copy; {{ date('Y') }}. Pour toutes questions concernant l'utilisation de la plateforme, contactez-nous à l'adresse mail suivante : <a href="mailto:info@akhaiacosmetics.com">info@akhaiacosmetics.com</a>
+    <div class="footer-left">
+        <p class="m-0">Pour toutes questions concernant l'utilisation de la plateforme, contactez-nous à l'adresse mail suivante : <a href="mailto:info@akhaiacosmetics.com">info@akhaiacosmetics.com</a></p>
+        <p class="m-0">Copyright &copy; {{ date('Y') }}.</p>
     </div>
     <div class="footer-right"></div>
 </footer>
